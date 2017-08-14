40c15605d99d2ebbb9c1a4842a27ea3204f8d5d3
Irvin cossio chavalier <irvin373@Irene.local>
2017-08-14 01:11:12 -0400

    initial commit

---
+++ src/org/tensorflow/demo/RecognitionScoreView.java
@@ -0,0 +1,96 @@
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package org.tensorflow.demo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import java.util.concurrent.TimeUnit;
+
+
+import org.tensorflow.demo.Classifier.Recognition;
+import android.media.MediaPlayer;
+import android.content.res.AssetFileDescriptor;
+import java.util.List;
+
+public class RecognitionScoreView extends View implements ResultsView {
+  private static final float TEXT_SIZE_DIP = 24;
+  private List<Recognition> results;
+  private final float textSizePx;
+  private final Paint fgPaint;
+  private final Paint bgPaint;
+  private static final String MODEL_FILE = "file:///android_asset/pikachu.mp3";
+
+  public RecognitionScoreView(final Context context, final AttributeSet set) {
+    super(context, set);
+
+    textSizePx =
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
+    fgPaint = new Paint();
+    fgPaint.setTextSize(textSizePx);
+
+    bgPaint = new Paint();
+    bgPaint.setColor(0xccFB0505);
+  }
+
+  private void startSound(){
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    try {
+      AssetFileDescriptor afd = getContext().getAssets().openFd("pikachu.mp3");
+      mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+      afd.close();
+      mediaPlayer.prepare();
+      mediaPlayer.start();
+    }
+    catch (Exception e){
+      System.out.println(e);
+    }
+  }
+
+  @Override
+  public void setResults(final List<Recognition> results) {
+    this.results = results;
+    postInvalidate();
+  }
+
+  @Override
+  public void onDraw(final Canvas canvas) {
+    final int x = 10;
+    int y = (int) (fgPaint.getTextSize() * 1.5f);
+
+    canvas.drawPaint(bgPaint);
+
+    if (results != null) {
+      for (final Recognition recog : results) {
+        canvas.drawText(recog.getTitle() + ": " + recog.getConfidence(), x, y, fgPaint);
+        if(recog.getTitle().toString().equals("pikachu") && recog.getConfidence() > 0.5 ){//play mp3
+          startSound();
+          try {
+            TimeUnit.SECONDS.sleep(3);
+          }catch (Exception e){
+            System.out.println(e);
+          }
+
+        }
+        y += fgPaint.getTextSize() * 1.5f;
+      }
+    }
+  }
+}
